aea1e770d817333fe01ae41594b9f72a9682a5e9
Oyatillo Meliboyev <92875679+swift8d@users.noreply.github.com>
2022-04-05 23:38:08 +0500

    upd delete dependencies

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ const cors = require('cors')
 const index = require('./router/index')
 const http = require('http')
 const app = express()
-const morgan = require('morgan')
 const httpServer = http.createServer(app)
 const connectionString = process.env.CONNECTION_STRING
 
 app.use(cors())
-app.use(morgan('combined'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 mongoose.connect(connectionString, {
